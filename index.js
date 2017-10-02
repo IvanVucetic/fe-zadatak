0c88f523a257ef93862225801fdacc292796a72e
Ivan Vucetic <vuceta87@yahoo.com>
2017-10-02 16:22:33 +0200

    grabbing the string from input field

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function handleSubmit(event) {
   // prevent page from reloading when form is submitted
   event.preventDefault();
   const input = document.querySelector(".searchField-input").value;
-  console.log(input);
+  console.log(input); //delete this later
   form.reset();
 }
+
+
+const data = JSON.parse('data.json');
+console.log(data);
