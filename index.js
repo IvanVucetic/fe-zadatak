bf5040b811bb7fa53bad1ae366caa8da0a8b18cf
Ivan Vucetic <vuceta87@yahoo.com>
2017-10-02 18:12:41 +0200

    function that fetches JSON

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ function handleSubmit(event) {
   form.reset();
 }
 
+// get the JSON data from a file
+function getJSON(url) {
+  var x = new XMLHttpRequest();
+  x.onreadystatechange = function(){
+    if(x.status == 200){
+      if (x.readyState == XMLHttpRequest.DONE) {
+        // console.log(x.responseText);
+        var a = x.response;   //object
+        alert(a["cars"][0]["name"]);   //fiat punto - works!
+      }
+    }
+  };
+      //other methods can be implemented here
+  x.open('GET', url);
+  x.responseType = 'json';
+  x.send();
+}
 
-const data = JSON.parse('data.json');
-console.log(data);
+getJSON('/data.json');
